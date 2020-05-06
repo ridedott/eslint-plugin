0d1b41b2e6fa7c18ddca54619c872d55e5350a02
Andrés C. Viesca Ruiz <andres.viescar@gmail.com>
2020-05-06 14:08:50 +0200

    fix: change version lookup

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,19 @@
 /* eslint-disable new-cap */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { ESLintUtils } from '@typescript-eslint/experimental-utils';
 import { parse as parsePath } from 'path';
 
-import { version } from '../package.json';
+/**
+ * This is not an import statement because it will make TSC copy the
+ * package.json to the dist folder.
+ */
+const { version } = require('../package.json');
 
 const REPO_URL = 'https://github.com/ridedott/eslint-config';
 
 export const createRule = ESLintUtils.RuleCreator((name: string): string => {
   const ruleName = parsePath(name).name;
 
-  return `${REPO_URL}/blob/v${version}/docs/rules/${ruleName}.md`;
+  return `${REPO_URL}/blob/v${version as string}/docs/rules/${ruleName}.md`;
 });
