457282183cdd29a439ea118cb3b1a83a2c19b408
Andrés C. Viesca Ruiz <andres.viescar@gmail.com>
2020-05-28 17:30:52 +0200

    fix: remove type assumption

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
  * @type {import('@jest/types').Config.InitialOptions}
  */
 module.exports = {
-  coveragePathIgnorePatterns: ['/node_modules/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/src/util.ts'],
   coverageReporters: ['lcov', 'text', 'text-summary'],
   coverageThreshold: {
     global: {
--- src/util.ts
+++ src/util.ts
@@ -10,12 +10,14 @@ import { parse as parsePath } from 'path';
  */
 const { version } = require('../package.json');
 
+if (typeof version !== 'string') {
+  throw new Error('Version field in package.json is not a string');
+}
+
 const REPO_URL = 'https://github.com/ridedott/eslint-config';
 
 export const createRule = ESLintUtils.RuleCreator((name: string): string => {
   const ruleName = parsePath(name).name;
 
-  return `${REPO_URL}/blob/v${
-    version as string
-  }/documentation/rules/${ruleName}.md`;
+  return `${REPO_URL}/blob/v${version}/documentation/rules/${ruleName}.md`;
 });
